280bb57c8a87e54014473e768c008cb807ae540f
Adam Bernacki <a.bernacki99@gmail.com>
2025-02-06 20:58:06 +0100

    refactored campaign management classes

--- marketing-api/src/main/java/com/bernacki/testtask/marketing_api/controller/CampaignController.java
+++ marketing-api/src/main/java/com/bernacki/testtask/marketing_api/controller/CampaignController.java
@@ -1,12 +1,7 @@
 package com.bernacki.testtask.marketing_api.controller;
-
 import com.bernacki.testtask.marketing_api.entity.Campaign;
-import com.bernacki.testtask.marketing_api.entity.Keyword;
-import com.bernacki.testtask.marketing_api.repository.CampaignRepository;
-import com.bernacki.testtask.marketing_api.repository.KeywordRepository;
 import com.bernacki.testtask.marketing_api.service.CampaignService;
-import com.bernacki.testtask.marketing_api.service.KeywordService;
-import com.bernacki.testtask.marketing_api.service.TownService;
+
 import jakarta.transaction.Status;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
@@ -23,67 +18,36 @@ import java.util.*;
 @RequestMapping("/api/campaigns")
 @RequiredArgsConstructor
 public class CampaignController {
-    private final CampaignRepository campaignRepository;
-    private final KeywordRepository keywordRepository;
-    private final KeywordService keywordService;
     private final CampaignService campaignService;
-    private final TownService townService;
+
 
     @GetMapping
     public List<Campaign> getAllCampaigns(){
-        return campaignRepository.findAll();
+        return campaignService.findAll();
     }
 
     @GetMapping("/{id}")
     public Campaign getCampaignById(@PathVariable BigInteger id){
-        return campaignRepository.findById(id).orElseThrow(() -> new RuntimeException("There is no campaign with such ID"));
+        return campaignService.findById(id);
     }
 
     @Transactional
     @PostMapping("/add")
     public ResponseEntity<HttpStatus> addNewCampaign(@Valid @RequestBody Campaign campaign){
-        if (campaignService.isNameUnique(campaign.getCampaignName())) {
-            try {
-                campaign = campaignService.assignTown(campaign);
-                campaignService.saveCampaign(campaign);
-                return ResponseEntity.status(HttpStatus.ACCEPTED).build();
-            } catch (RuntimeException e) {
-                return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-            }
-        }
-        return ResponseEntity.status(HttpStatus.CONFLICT).build();
+        return campaignService.checkCampaignCorrectness(campaign);
     }
 
     @Transactional
     @PutMapping("/edit/{id}")
     public ResponseEntity<HttpStatus> editCampaign(@Valid @RequestBody Campaign modifiedCampaign, @PathVariable BigInteger id) {
-        if (!campaignRepository.existsById(id)) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-        }
-
-        Campaign originalCampaign = campaignRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("There is no campaign with such ID"));
-
-        if (!originalCampaign.getCampaignName().equals(modifiedCampaign.getCampaignName()) &&
-                !campaignService.isNameUnique(modifiedCampaign.getCampaignName())) {
-            return ResponseEntity.status(HttpStatus.CONFLICT).build();
-        }
-
-        try {
-            modifiedCampaign.setId(id);
-            modifiedCampaign = campaignService.assignTown(modifiedCampaign);
-            campaignService.saveCampaign(modifiedCampaign);
-            return ResponseEntity.status(HttpStatus.ACCEPTED).build();
-        } catch (RuntimeException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-        }
+        return campaignService.editCampaign(modifiedCampaign, id);
     }
 
 
     @Transactional
     @DeleteMapping("/delete/{id}")
     public void deleteCampaign(@PathVariable BigInteger id){
-        campaignRepository.deleteById(id);
+        campaignService.deleteById(id);
     }
 
 }
--- marketing-api/src/main/java/com/bernacki/testtask/marketing_api/service/CampaignService.java
+++ marketing-api/src/main/java/com/bernacki/testtask/marketing_api/service/CampaignService.java
@@ -8,10 +8,14 @@ import com.bernacki.testtask.marketing_api.repository.KeywordRepository;
 import com.bernacki.testtask.marketing_api.repository.TownRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.math.BigInteger;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -49,5 +53,51 @@ public class CampaignService {
             }
         }
         return true;
+    }
+
+    public ResponseEntity<HttpStatus> checkCampaignCorrectness(Campaign campaign){
+        if (isNameUnique(campaign.getCampaignName())) {
+            try {
+                campaign = assignTown(campaign);
+                saveCampaign(campaign);
+                return ResponseEntity.status(HttpStatus.ACCEPTED).build();
+            } catch (RuntimeException e) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+            }
+        }
+        return ResponseEntity.status(HttpStatus.CONFLICT).build();
+    }
+
+    public ResponseEntity<HttpStatus> editCampaign(Campaign modifiedCampaign, BigInteger id){
+        if (!checkIfExistById(id)) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+
+        Campaign originalCampaign = findById(id);
+        modifiedCampaign.setId(id);
+        if(!originalCampaign.getCampaignName().equals(modifiedCampaign.getCampaignName())){
+            return checkCampaignCorrectness(modifiedCampaign);
+        }
+        else{
+            modifiedCampaign = assignTown(modifiedCampaign);
+            saveCampaign(modifiedCampaign);
+        }
+        return ResponseEntity.status(HttpStatus.ACCEPTED).build();
+    }
+
+    public List<Campaign> findAll(){
+        return campaignRepository.findAll();
+    }
+
+    public Campaign findById(BigInteger id){
+        return campaignRepository.findById(id).orElseThrow(() -> new RuntimeException("There is no campaign with such ID"));
+    }
+
+    public boolean checkIfExistById(BigInteger id){
+        return campaignRepository.existsById(id);
+    }
+
+    public void deleteById(BigInteger id){
+        campaignRepository.deleteById(id);
     }
 }
